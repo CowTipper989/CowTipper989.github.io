a3e0c895f01abe5285defa64a189997b36edfe4a
CowTipper989 <wellsluke88@tuta.io>
2022-07-09 12:53:14 -0500

    React Components Initialized

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
 import React from 'react'
 
+import { Footer, Blog, Possibility, Features, WhatPortfolio, Header } from './containers';
+import { CTA, Brand, Navbar } from './components';
+
 const App = () => {
   return (
-    <div>
-        <h1>MyWebsite</h1>
+    <div className="App">
+        <div className="gradient__bg">
+          <Navbar />
+          <Header />
+        </div>
+        <Brand />
+        <WhatPortfolio />
+        <Features />
+        <Possibility />
+        <CTA />
+        <Blog />
+        <Footer />
     </div>
   )
 }
