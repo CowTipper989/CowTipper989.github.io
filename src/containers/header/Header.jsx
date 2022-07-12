cdf10a89b80e79e9bd04392013581d15ec62f536
CowTipper989 <wellsluke88@tuta.io>
2022-07-11 22:15:12 -0500

    Added todo, fixed nav menu

--- src/containers/header/Header.jsx
+++ src/containers/header/Header.jsx
@@ -9,7 +9,10 @@ const Header = () => {
             Luke's Portfolio
           </h1>
           <p>
-            Hello! Welcome to my portfolio. This website will host some of my projects, and also my work experience and education.
+            Hello! Welcome to my portfolio.
+          </p>
+          <p>
+          This website will host some of my projects, and also my work experience and education.
           </p>
         </div>
     </div>
--- src/containers/projects/Projects.jsx
+++ src/containers/projects/Projects.jsx
@@ -3,7 +3,7 @@ import './projects.css';
 
 const Projects = () => {
   return (
-    <div>
+    <div id="projects">
         Projects
     </div>
   )
